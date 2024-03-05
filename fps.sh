0272eca85227179f71351e9e4ef684a5a7790eb1
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-03-06 00:15:00 +0700

    Update fps.sh

--- fps.sh
+++ fps.sh
@@ -8,6 +8,8 @@ disable='
 java -jar apktool/apktool.jar d PowerKeeper.apk -o tmp/
 for nmethod in $(grep -rn ".method public setScreenEffect" "tmp/smali")
 do
-  echo $nmethod
+  replace1=`cut -d ':' -f 1 $nmethod`
+  replace2=`cut -d ':' -f 2 $nmethod`
+  echo $replace1 $replace2
 done
 java -jar apktool/apktool.jar b -f "tmp/" -o "tmp/"
