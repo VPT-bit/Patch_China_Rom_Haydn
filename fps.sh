e25b57b3816c89a49f7fe2d5ccea9c6c3c48d306
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-03-05 22:58:35 +0700

    Update fps.sh

--- fps.sh
+++ fps.sh
@@ -5,7 +5,5 @@ disable='
 
     return-void
 '
-apktool d PowerKeeper.apk -o tmp/
-java -jar apktool/smali.jar -check -method "setScreenEffect" -remake "$disable" -dir "tmp/"
-java -jar apktool/smali.jar -check -method "setScreenEffectInternal" -remake "$disable" -dir "tmp/"
-apktool b "tmp/" -o .
+java -jar apktool/apktool.jar d PowerKeeper.apk -o tmp/
+java -jar apktool/apktool.jar b -f "tmp/"
