0bae9be4c2e2d8a9922797189d9c94dac56a8165
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-08 16:39:24 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@ dir=$(pwd)
 sudo chmod 777 -R *
 apkpath=$(find . -type f -name *.apk)
 apktool d $apkpath -o tmp
+sed -i "s/Lmiui\/os\/Build;->IS_INTERNATIONAL_BUILD:Z/Lmiuix\/os\/Build;->IS_INTERNATIONAL_BUILD:Z/g" "tmp/smali/miuix/os/Build.smali"
