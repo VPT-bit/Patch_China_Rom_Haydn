1925abceae122a3f2df63d700b0ff99a00e5093b
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-09 14:10:41 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -14,5 +14,5 @@ patch_method()
 apkpath=$(find . -type f -name *.apk)
 apktool d $apkpath -o tmp
 patch_method "Lmiui\/os\/Build;->IS_INTERNATIONAL_BUILD:Z" "Lmiuix\/os\/Build;->IS_INTERNATIONAL_BUILD:Z"
-java -jar bin/apktool/apktool_2.9.3.jar b tmp -o PowerKeeper_compiled.apk
-./bin/apktool/zipalign -p -f -v 4 PowerKeeper_compiled.apk PowerKeeper_note.apk
+java -jar bin/apktool/apktool_2.9.3.jar b tmp -o PowerKeeper_compiled.tmp
+./bin/apktool/zipalign -p -v 4 PowerKeeper_compiled.tmp PowerKeeper_note.apk
