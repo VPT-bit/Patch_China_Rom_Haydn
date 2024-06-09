606124e7cb3eb4d1fe3421046440acbb2fa3466b
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-09 13:22:02 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -14,4 +14,4 @@ patch_method()
 apkpath=$(find . -type f -name *.apk)
 apktool d $apkpath -o tmp
 patch_method "Lmiui\/os\/Build;->IS_INTERNATIONAL_BUILD:Z" "Lmiuix\/os\/Build;->IS_INTERNATIONAL_BUILD:Z"
-apktool b tmp -o tmp
+apktool b tmp -o PowerKeeper_patched.apk
