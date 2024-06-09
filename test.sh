10782648e9e54285b51751bb0561e683a52c1f00
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-09 13:00:10 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 dir=$(pwd)
 sudo chmod 777 -R *
+patch_method()
+{
+  for filesmali in $(find tmp/smali -type f -name *.smali);
+  do
+    if grep -q "$1" "filesmali"; then
+      echo Patching $filesmali...
+      sed -i "s/$1/$2/g" "$filesmali" && echo Patched $filesmali || echo Error
+    fi
+  done
 apkpath=$(find . -type f -name *.apk)
 apktool d $apkpath -o tmp
-sed -i "s/Lmiui\/os\/Build;->IS_INTERNATIONAL_BUILD:Z/Lmiuix\/os\/Build;->IS_INTERNATIONAL_BUILD:Z/g" "tmp/smali/miuix/os/Build.smali" && echo "done"
+patch_method "Lmiui\/os\/Build;->IS_INTERNATIONAL_BUILD:Z" "Lmiuix/os/Build;->IS_INTERNATIONAL_BUILD:Z"
