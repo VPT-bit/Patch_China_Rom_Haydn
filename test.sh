cf1243d6b707b056478d1c410a9c47caced1c5e2
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-09 14:44:52 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,15 +5,15 @@ patch_method()
 {
   stock_file=$(find . -type f -name "$1")
   [ -f $stock_file ] && echo Found $1 in $stock_file || echo Not found $1
-  java -jar bin/apktool/apktool_2.9.3.jar d $stock_file -o tmp && echo Decompile $1 successfully || echo Failed to decompile $1 > /dev/null 2>&1
+  java -jar bin/apktool/apktool_2.9.3.jar d $stock_file -o tmp > /dev/null 2>&1 && echo Decompile $1 successfully || echo Failed to decompile $1
   for filesmali in $(find tmp/smali -type f -name *.smali);
   do
     if grep -q "$2" "$filesmali"; then
       sed -i "s/$2/$3/g" "$filesmali" && echo Patched $filesmali || echo Error
     fi
   done
-  java -jar bin/apktool/apktool_2.9.3.jar b tmp -o $1.recompile && echo Compile $1 successfully || echo Failed to compile $1 > /dev/null 2>&1
-  zipalign -p -v 4 $1.recompile $1 && echo Zipalign successfully || echo Failed to zipalign > /dev/null 2>&1
+  java -jar bin/apktool/apktool_2.9.3.jar b tmp -o $1.recompile > /dev/null 2>&1 && echo Compile $1 successfully || echo Failed to compile $1
+  zipalign -p -v 4 $1.recompile $1 > /dev/null 2>&1 && echo Zipalign successfully || echo Failed to zipalign
   rm -rf tmp/*
 }
 patch_method "PowerKeeper.apk" "Lmiui\/os\/Build;->IS_INTERNATIONAL_BUILD:Z" "Lmiuix\/os\/Build;->IS_INTERNATIONAL_BUILD:Z"
