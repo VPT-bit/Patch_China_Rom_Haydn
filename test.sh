604edf3a45393bec836c3edda18ca39aade39cd3
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-19 02:58:14 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,10 +11,10 @@ patch_fps_limit()
   path="$1"
   name=$(basename "$path")
   [ -f $path ] && green "Found $name" || error "Not found $name"
-  7z x -y "$path" *.dex -o tmp/dex
+  7z x -y "$path" *.dex -otmp/dex
   for dex_file in tmp/dex/*.dex; do
             dex_name=$(basename $dex_file | cut -d '.' -f 2)
-            baksmali d --api 34 ${dex_file} -otmp/dex/$dex_name > /dev/null 2>&1 && green "baksmaling $dex_name done" || error "baksmaling failed"
+            baksmali d --api 34 ${dex_file} -o tmp/dex/$dex_name > /dev/null 2>&1 && green "baksmaling $dex_name done" || error "baksmaling failed"
   done
   for file_smali in $(find tmp/dex -type f -name *.smali);
   do
