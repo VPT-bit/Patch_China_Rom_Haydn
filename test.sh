bf347c26602e2278e53a75fd9d998ed850216edd
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-18 10:35:21 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -17,7 +17,4 @@ patch_method()
   zipalign -p -v 4 output/$1.recompile output/$1 > /dev/null 2>&1 && green "Zipalign successfully" || error "Failed to zipalign"
   rm -rf tmp/*
 }
-git clone https://github.com/buihien224/overlay.git
-cd overlay
-sudo chmod 777 -R *
-./build.sh "all"
+sudo python3 test.py
