c6a1b7c82e8a4d1346e623d2dcce6599156e0757
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-19 00:37:42 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,47 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
+
 def replace_method_content(file_path, method_name, new_content):
-    # Đọc nội dung của file smali
     with open(file_path, 'r') as file:
         file_content = file.read()
 
-    # Tách các method bằng cách tìm từ khóa '.method'
     methods = file_content.split('.method')
 
-    # Tìm method cần thay thế
-    for i in range(1, len(methods)):  # bắt đầu từ 1 vì methods[0] là phần trước từ khóa đầu tiên
+    for i in range(1, len(methods)):
         method = methods[i]
-        # Tìm dấu kết thúc method
         end_index = method.find('.end method')
         if end_index != -1:
-            # Lấy tên của method từ ".method" đến kí tự đầu tiên là khoảng trắng
             method_header = method[:end_index]
             method_name_start = method_header.find(method_name)
             if method_name_start != -1:
-                # Tìm thấy method cần thay thế
-                # Tìm vị trí bắt đầu và kết thúc của nội dung method (giữa .method và .end method)
                 method_start_index = method.find('\n', method_name_start) + 1
                 method_end_index = end_index
 
-                # Thay thế nội dung trong method này
                 new_method_content = method[:method_start_index] + new_content + method[method_end_index:]
                 methods[i] = new_method_content
                 break
 
-    # Ghép lại nội dung file
     new_file_content = '.method'.join(methods)
 
-    # Ghi lại vào file
     with open(file_path, 'w') as file:
         file.write(new_file_content)
 
-# Sử dụng hàm replace_method_content để thay đổi nội dung của method trong file test.smali
-file_path = 'test.smali'
-method_name_to_replace = 'displayControl'
-new_method_content = '''
-    .registers 4
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: python script.py <file_path> <method_name_to_replace> <new_method_content>")
+        sys.exit(1)
 
-    return-void
-'''
+    file_path = sys.argv[1]
+    method_name_to_replace = sys.argv[2]
+    new_method_content = sys.argv[3]
 
-replace_method_content(file_path, method_name_to_replace, new_method_content)
+    replace_method_content(file_path, method_name_to_replace, new_method_content)
