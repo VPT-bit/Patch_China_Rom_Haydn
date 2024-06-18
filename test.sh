75aa8030fa8e65004bec5af4d337350fabddcedf
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-19 01:56:59 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,24 +6,26 @@ disable="
 
     return-void
 "
-patch_method()
+patch_fps_limit()
 {
   path="$1"
   name=$(basename "$path")
-  method="$2"
-  new_context="$3"
   [ -f $path ] && green "Found $name" || error "Not found $name"
   java -jar bin/apktool/apktool_2.9.3.jar d $path -o tmp > /dev/null 2>&1 && green "Decompile $name successfully" || error "Failed to decompile $name"
   for file_smali in $(find tmp -type f -name *.smali);
   do
-    if grep -q "$method" "$file_smali"; then
-      sudo python3 remake_method.py "$file_smali" "$method" "$new_context" && green "Patched method $method in $file_smali" || error "Failed to patch method $method"
+    if grep -q "setScreenEffect" "$file_smali"; then
+      sudo python3 remake_method.py "$file_smali" "setScreenEffect" "$disable" && green "Patched method setScreenEffect in $file_smali" || error "Failed to patch method setScreenEffect"
+    fi
+    
+    if grep -q "setScreenEffectInternal" "$file_smali"; then
+      sudo python3 remake_method.ly "$file_smali" "setScreenEffectInternal" "$disable" && green "Patched method setScreenEffectInternal in $file_smali" || error "Failed to patch method setScreenEffectInternal"
     fi
   done
   mkdir -p output
   java -jar bin/apktool/apktool_2.9.3.jar b tmp -o output/$name.recompile > /dev/null 2>&1 && green "Compile $name successfully" || error "Failed to compile $name"
   zipalign -p -v 4 output/$name.recompile output/$name > /dev/null 2>&1 && green "Zipalign successfully" || error "Failed to zipalign"
-  apksigner sign output/$name && green "signed" || error "fail to sign"
+  apksigner verify output/$name && green "signed" || error "fail to sign"
   rm -rf tmp/*
 }
 
