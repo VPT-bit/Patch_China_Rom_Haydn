402cb50e5e3e001ee15e23b13745f97c6e90d73b
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-18 11:09:27 +0700

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ def replace_method_content(file_path, method_name, replacement_text):
         output_lines = []
         method_found = False
         for line in lines:
-            
             if not method_found and re.match(r'\.method[^\n]*{}\(.*\)'.format(re.escape(method_name)), line):
                 method_found = True
                 output_lines.append(line)
@@ -25,7 +24,7 @@ def replace_method_content(file_path, method_name, replacement_text):
             else:
                 output_lines.append(line)
 
-        with open(file_path, 'w', encoding='utf-8') as file:
+        with open(file_path + '.new', 'w', encoding='utf-8') as file:
             file.writelines(output_lines)
 
         print(f'Đã thay thế nội dung của method {method_name} thành công.')
@@ -42,9 +41,11 @@ if __name__ == "__main__":
 
     file_path = sys.argv[2]
     method_name = sys.argv[1]
+
     replacement_text = (
-        '\t.register 4\n' +
+        '\t.registers 4\n' +
         '\n\treturn-void\n' +
         '.end method\n'
     )
+
     replace_method_content(file_path, method_name, replacement_text)
