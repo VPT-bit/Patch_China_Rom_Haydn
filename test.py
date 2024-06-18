884d07b89a6866ab29a21b4f8b8db54dc5dbf29c
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-19 00:25:38 +0700

    Update test.py

--- test.py
+++ test.py
@@ -19,16 +19,13 @@ def replace_method_content(file_path, method_name, new_content):
             method_name_start = method_header.find(method_name)
             if method_name_start != -1:
                 # Tìm thấy method cần thay thế
-                # Tìm dòng chứa tên method và kí tự bất kỳ cuối cùng
-                method_line_start = method.rfind('\n', 0, method_name_start) + 1
-                method_line_end = method.find('\n', method_name_start) + 1
-                method_line = method[method_line_start:method_line_end]
+                # Tìm vị trí bắt đầu và kết thúc của nội dung method (giữa .method và .end method)
+                method_start_index = method.find('\n', method_name_start) + 1
+                method_end_index = end_index
 
                 # Thay thế nội dung trong method này
-                new_method = method[:method_name_start + len(method_name)] + new_content + method[end_index:]
-                # Giữ nguyên dòng chứa tên method và kí tự bất kỳ cuối cùng
-                new_method = new_method.replace(new_content, method_line.strip() + new_content)
-                methods[i] = new_method
+                new_method_content = method[:method_start_index] + new_content + method[method_end_index:]
+                methods[i] = new_method_content
                 break
 
     # Ghép lại nội dung file
@@ -38,7 +35,7 @@ def replace_method_content(file_path, method_name, new_content):
     with open(file_path, 'w') as file:
         file.write(new_file_content)
 
-# Sử dụng hàm replace_method_content để thay thế nội dung của method trong file test.smali
+# Sử dụng hàm replace_method_content để thay đổi nội dung của method trong file test.smali
 file_path = 'test.smali'
 method_name_to_replace = 'parseTopSmartAppFromDb'
 new_method_content = '''
