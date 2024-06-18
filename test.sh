99d84c45a424147dcae8abc9b9b3fdb5c25ca3a2
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-18 20:13:49 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -17,4 +17,4 @@ patch_method()
   zipalign -p -v 4 output/$1.recompile output/$1 > /dev/null 2>&1 && green "Zipalign successfully" || error "Failed to zipalign"
   rm -rf tmp/*
 }
-sudo python3 test.py 'parseTopSmartAppFromDb' 'test.txt'
+sudo python3 test.py "parseTopSmartAppFromDb" "test.txt" "\n\t.registers 4\n\n\treturn-void\n"
