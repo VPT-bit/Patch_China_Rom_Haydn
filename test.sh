adac041dc6ee93d595566231c2b2e591387d8715
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-06-19 08:48:15 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -21,7 +21,7 @@ patch_fps_limit()
     sudo python3 remake_method.py "$file_smali" "setScreenEffect" "$disable" && green "Patched method setScreenEffect in $file_smali" || error "Failed to patch method setScreenEffect"
     sudo python3 remake_method.py "$file_smali" "setScreenEffectInternal" "$disable" && green "Patched method setScreenEffectInternal in $file_smali" || error "Failed to patch method setScreenEffectInternal"
     mkdir -p output
-    java -jar bin/apktool/apktool_2.9.3.jar b -c tmp -o output/$name.out > /dev/null 2>&1 && green "compiling done" || error "failed to compile"
+    java -jar bin/apktool/apktool_2.9.3.jar b -c tmp -o output/$name.out && green "compiling done" || error "failed to compile"
     zipalign -p -f -v 4 output/$name.out output/$name > /dev/null 2>&1 && green "Zipalign successfully" || error "Failed to zipalign"
     apksigner sign output/$name > /dev/null 2>&1 && green "signed" || error "fail to sign"
     if [ ! -f output/$name ]; then
