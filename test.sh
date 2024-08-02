4c4d627fbaa4604cf02bbdad27d8cb94d5cc6a5f
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-08-02 17:20:16 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 java -jar bin/apktool/apktool.jar -r -b -f d -c -api 34 --use-aapt2 PowerKeeper.apk -o tmp/PowerKeeper
 VOID='.locals 1
     return-void'
-python3 test.py "tmp/PowerKeeper/smali/com/miui/powerkeeper/statemachine/DisplayFrameSetting.smali" "setScreenEffect" "$VOID"
+python3 test.py "test.smali" "displayControl" "$VOID"
