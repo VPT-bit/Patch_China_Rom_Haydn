a068adf57c205d337ba6f684a24df12fbcbb1c1a
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-09-05 00:53:46 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 void='.locals 1
     return-void'
-python3 test.py test.smali setScreenEffect "$void"
+    
+FALSE='.locals 1
+    const/4 p0, 0x0
+    return p0'
+    
+python3 test.py test.smali "setScreenEffect" "$void"
+python3 test.py test.smali "setScreenEffectInternal" "$void"
+python3 test.py test.smali "getDisplayCtrlCode" "$FALSE"
