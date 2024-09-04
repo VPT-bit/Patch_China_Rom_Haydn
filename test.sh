56e9056dc5e4f341a857b7d5c1ad2a2500024332
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-09-04 22:40:33 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
-java -jar bin/apktool/apktool.jar -r -b -f d -c -api 34 --use-aapt2 PowerKeeper.apk -o tmp/PowerKeeper
-VOID='.locals 1
+disable='.registers 4
     return-void'
-python3 test.py "test.smali" "parseTopSmartAppFromDb" "$VOID"
+python3 test.py "test.smali" "parseTopSmartAppFromDb" "$disable"
