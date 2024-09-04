6b2e98aac8f74cf10370957004e756f63e769b26
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-09-05 01:47:59 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,16 +4,10 @@ VOID='.locals 1
 FALSE='.locals 1
     const/4 p0, 0x0
     return p0'
-
+target_dir="PowerKeeper/smali/com/miui/powerkeeper"
 java -jar bin/apktool/apktool_2.9.3.jar d PowerKeeper.apk -o PowerKeeper
-for file in $(find PowerKeeper/smali/com/miui/powerkeeper -type f -name *.smali); do
-    python3 test.py "$file" "setScreenEffect" "$VOID"
-done
-for file in $(find PowerKeeper/smali/com/miui/powerkeeper -type f -name *.smali); do
-    python3 test.py "$file" "setScreenEffectInternal" "$VOID"
-done
-for file in $(find PowerKeeper/smali/com/miui/powerkeeper -type f -name *.smali); do
-    python3 test.py "$file" "getDisplayCtrlCode" "$FALSE"
-done
+python3 test.py "$target_dir/statemachine/DisplayFrameSetting.smali" "setScreenEffect" "$VOID"
+python3 test py "$target_dir/statemachine/DisplayFrameSetting.smali" "setScreenEffectInternal" "$VOID"
+python3 test.py "$target_dir/feedbackcontrol/ThermalManager.smali" "getDisplayCtrlCode" "$FALSE"
 java -jar bin/apktool/apktool_2.9.3.jar b PowerKeeper -o PowerKeeper_patched_raw.apk
 zipalign -p -v 4 PowerKeeper_patched_raw.apk PowerKeeper_patched.apk
