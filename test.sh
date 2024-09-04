9f1b2dbb30bcd87fab2b77013a379d81cdc2d6cf
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-09-05 01:48:16 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ FALSE='.locals 1
 target_dir="PowerKeeper/smali/com/miui/powerkeeper"
 java -jar bin/apktool/apktool_2.9.3.jar d PowerKeeper.apk -o PowerKeeper
 python3 test.py "$target_dir/statemachine/DisplayFrameSetting.smali" "setScreenEffect" "$VOID"
-python3 test py "$target_dir/statemachine/DisplayFrameSetting.smali" "setScreenEffectInternal" "$VOID"
+python3 test.py "$target_dir/statemachine/DisplayFrameSetting.smali" "setScreenEffectInternal" "$VOID"
 python3 test.py "$target_dir/feedbackcontrol/ThermalManager.smali" "getDisplayCtrlCode" "$FALSE"
 java -jar bin/apktool/apktool_2.9.3.jar b PowerKeeper -o PowerKeeper_patched_raw.apk
 zipalign -p -v 4 PowerKeeper_patched_raw.apk PowerKeeper_patched.apk
