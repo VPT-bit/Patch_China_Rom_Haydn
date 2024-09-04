fadd8a0e6969b2111d05c7894b6d9e2ecb0050a0
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-09-05 01:16:59 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,19 @@
-void='.locals 1
+VOID='.locals 1
     return-void'
     
 FALSE='.locals 1
     const/4 p0, 0x0
     return p0'
-    
-python3 test.py test.smali "setScreenEffect" "$void"
-python3 test.py test.smali "setScreenEffectInternal" "$void"
-python3 test.py test.smali "cc" "$FALSE"
+
+java -jar bin/apktool/apktool_2.9.3.jar d PowerKeeper.apk -o PowerKeeper
+for file in $(find PowerKeeper/smali/com/miui/powerkeeper -type f -name *.smali); do
+    python3 test.py "$file" "setScreenEffect" "$VOID"
+done
+for file in $(find PowerKeeper/smali/com/miui/powerkeeper -type f -name *.smali); do
+    python3 test.py "$file" "setScreenEffectInternal" "$VOID"
+done
+for file in $(find PowerKeeper/smali/com/miui/powerkeeper -type f -name *.smali); do
+    python3 test.py "$file" "getDisplayCtrlCode" "$FALSE"
+done
+java -jar bin/apktool/apktool_2.9.3.jar b PowerKeeper -o PowerKeeper_patched_raw.apk
+./bin/apktool/zipalign -p -v 4 PowerKeeper_patched_raw.apk PowerKeeper_patched.apk
