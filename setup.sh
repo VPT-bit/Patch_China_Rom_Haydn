fe05104087108d00ef6e390b9ca5c8077144b60c
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-11-25 16:25:57 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 su="sudo bash"
 
 $su download_latest.sh "ssut/payload-dumper-go" "linux_amd64"
-payload_dumper_go=$(find . -type f -name *linux_amd64.tar.gz)
+payload_dumper_go=$(find . -type f -name '*linux_amd64.tar.gz')
 if [ -f payload_dumper_go ]; then
     echo "PASS"
 else
