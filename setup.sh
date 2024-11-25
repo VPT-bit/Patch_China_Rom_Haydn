2343d4f8b1121f6d9425a674815267687a04df10
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-11-25 16:17:02 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+su="sudo bash"
 
-./download_latest.sh "ssut/payload-dumper-go" "linux_amd64"
+$su download_latest.sh "ssut/payload-dumper-go" "linux_amd64"
 payload_dumper_go=$(find . -type f -name *linux_amd64.tar.gz)
 [[ -f payload_dumper_go ]] && echo "PASS" || echo "FAIL"
